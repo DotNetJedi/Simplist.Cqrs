06b6a268b1159f26dee3bc828982f532aedf077e
Eric De Carufel <edecarufel@pyxis-tech.com>
2016-08-18 10:18:23 -0400

    Add test run and cover

---
+++ test-with-coverage.bat
@@ -0,0 +1,25 @@
+setlocal
+
+if "%BUILD_CONFIGURATION%" == "" set BUILD_CONFIGURATION=Debug
+set BUILD_TARGET=%BUILD_CONFIGURATION%
+set PATH=%~dp0\.node;%PATH%
+set NUNIT=packages\NUnit.ConsoleRunner.3.4.1\tools\nunit3-console.exe
+set OPENCOVER=packages\OpenCover.4.6.519\tools\OpenCover.Console.exe
+set REPORT_GEN=packages\ReportGenerator.2.4.5.0\tools\ReportGenerator.exe
+set TOCOBERTURA=packages\OpenCoverToCoberturaConverter.0.2.4.0\tools\OpenCoverToCoberturaConverter.exe
+set TEST_TARGETS=Core.Tests\bin\%BUILD_TARGET%\Simplist.Core.Tests.dll
+set NAMESPACE_FILTERS=+[*]* -[FluentAssertions*]*
+set ATTRIBUTE_FILTERS=*GeneratedCode*
+
+mkdir Reports\Coverage
+del /Q /S Reports
+
+%OPENCOVER% -register:user "-target:%NUNIT%" "-targetargs:%TEST_TARGETS% --result:Reports\TestResults.xml;format=nunit2" -output:Reports\OpenCover.xml -filter:"%NAMESPACE_FILTERS%" -excludebyattribute:%ATTRIBUTE_FILTERS%
+
+rem to enable code coverage uncomment the two following lines
+%TOCOBERTURA% -input:Reports\OpenCover.xml -output:Reports\cobertura.xml -sources:.
+%REPORT_GEN% -reports:Reports\OpenCover.xml -targetdir:Reports\Coverage\Server
+
+rem call .\node_modules\.bin\karma.cmd start karma-jenkins.conf.js
+
+endlocal
